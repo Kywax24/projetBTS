c0d0a95a8390d758ee22be302991d46e01898374
Kywax24 <123954028+Kywax24@users.noreply.github.com>
2025-03-04 10:13:55 +0100

    Add files via upload

---
+++ Visual/TP2_Complet/exo2 -0 2/Exo_2/Exo_2/Module1.vb
@@ -0,0 +1,33 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim indice As Double
+        Dim salaire_brut As Double
+        Dim prix_point As Double
+        Dim taux_de_retenue As Double
+        prix_point = 3.25
+        taux_de_retenue = 0.2
+
+        Console.WriteLine("Calcul du salaire brut et net d'un fonctionnaire :")
+
+        Console.Write("Veuillez saisir le nombre de points : ")
+        indice = Console.ReadLine()
+        salaire_brut = prix_point * indice
+        Console.Write("L'indice est de : ")
+        Console.WriteLine(indice)
+        Console.Write("Le prix du point est de :")
+        Console.WriteLine(prix_point)
+        Console.Write("Le taux de retenue :")
+        Console.WriteLine(taux_de_retenue)
+        Console.Write("Le salaire brut est de : ")
+        Console.WriteLine(salaire_brut)
+        Console.Write("Le salaire net est de : ")
+        Console.WriteLine(salaire_brut * (1 - taux_de_retenue))
+        Console.ReadLine()
+
+
+
+    End Sub
+
+End Module
---
+++ Visual/TP2_Complet/exo2/exo2/Module1.vb
@@ -0,0 +1,26 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim indice As Int16
+        Dim salaireB As Double
+        Dim salaireN As Double
+        Dim tauxBrut As Double
+        Dim prixPoint As Double
+        prixPoint = 3.25
+        tauxBrut = 0.8
+
+        Console.WriteLine("Application qui calcule le salaire brut et le salaire net d'un fonctionnaire")
+        Console.WriteLine("Saisir l'indice :")
+        indice = Console.ReadLine()
+        salaireB = indice * prixPoint
+        salaireN = salaireB * tauxBrut
+        Console.WriteLine("Indice = " + Convert.ToString(indice))
+        Console.WriteLine("Prix du point = " + Convert.ToString(prixPoint))
+        Console.WriteLine("Taux de retenue = " + Convert.ToString((1 - tauxBrut)))
+        Console.WriteLine("Salaire brut = " + Convert.ToString(salaireB))
+        Console.WriteLine("Salaire net = " + Convert.ToString(salaireN))
+        Console.ReadLine()
+
+    End Sub
+
+End Module
