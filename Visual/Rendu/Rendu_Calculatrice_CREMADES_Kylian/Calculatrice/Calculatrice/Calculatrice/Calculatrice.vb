841cb0da19a88592699849a5eacfe406f74e94a4
Kywax24 <123954028+Kywax24@users.noreply.github.com>
2025-03-04 09:13:29 +0100

    Add files via upload

---
+++ Visual/Rendu/Rendu_Calculatrice_CREMADES_Kylian/Calculatrice/Calculatrice/Calculatrice/Calculatrice.vb
@@ -0,0 +1,22 @@
+﻿Module Calculatrice
+
+	Sub Main()
+		Dim nombre1 As Int16
+		Dim nombre2 As Int16
+
+		Console.WriteLine("Voici ma première calculatrice géniale ")
+
+		Console.Write("Premier nombre = ")
+		nombre1 = Console.ReadLine()
+
+		Console.Write("Deuxième nombre = ")
+		nombre2 = Console.ReadLine()
+
+
+		Console.Write("Le résultat est ")
+		Console.Write(nombre1 + nombre2)
+
+		Console.ReadLine()
+	End Sub
+
+End Module
